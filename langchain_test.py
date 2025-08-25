9e6ee44785c36f7dd999e6423d2a47ef5bc0be44
codecowboydotio <scottvankalken@gmail.com>
2025-08-25 20:32:30 +1000

    test

---
+++ langchain_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import getpass
+import os
+
+if not os.environ.get("ANTHROPIC_API_KEY"):
+  os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
+
+response = model.invoke("describe the difference between ise and ize word endings in english")
+print(response.content)
