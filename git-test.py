63614ca97dbaab5747e2ad63505f93603fc37aa8
codecowboydotio <scottvankalken@gmail.com>
2025-08-26 20:47:05 +1000

    updated git error handling

--- git-test.py
+++ git-test.py
@@ -13,6 +13,10 @@ if not os.environ.get("GIT_PAT_AI"):
 else:
   GIT_TOKEN=os.environ["GIT_PAT_AI"]
 
+if not os.environ.get("ANTHROPIC_API_KEY"):
+  os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
+
+
 url='https://api.github.com/repos/codecowboydotio/swapi-json-server/contents/'
 
 headers = {
@@ -52,10 +56,12 @@ json_schema = {
 
 structured_model = model.with_structured_output(json_schema)
 print("Initializing model...")
-response = structured_model.invoke(f'find dockerfiles in array {query_answer.text} return only value download_url')
+try:
+  response = structured_model.invoke(f'find dockerfiles in array {query_answer.text} return only value download_url')
+except Exception as e:
+  print(f"LLM error: {e}")
+  exit(1)
 print("Found dockerfile...")
-#print(response)
-#print(response["fileurl"])
 
 print("Pulling dockerfile from git....")
 dockerfile = requests.get(response["fileurl"])
@@ -66,4 +72,4 @@ print("Original: " + first_line)
 print("Sending entire dockerfile to LLM to determine latest baseimage...")
 response = model.invoke(f'Update the FROM command to be the latest baseimage version for {dockerfile.text}, return the updated dockerfile')
 print(response.content)
-
+print(response)
