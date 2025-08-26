92a2dbc45134d45caa0cfa6832d3171be8c21e3a
codecowboydotio <scottvankalken@gmail.com>
2025-08-26 16:50:33 +1000

    updates

--- git-test.py
+++ git-test.py
@@ -3,6 +3,10 @@
 import getpass
 import os
 import requests
+import json
+from langchain.chat_models import init_chat_model
+from typing import Optional
+from typing_extensions import Annotated, TypedDict
 
 if not os.environ.get("GIT_PAT_AI"):
   os.environ["GIT_PAT_AI"] = getpass.getpass("Enter API key for Github: ")
@@ -22,7 +26,44 @@ query_response_code = query_answer.status_code
 if query_response_code  == 204:
   print("Please check github actions for the status of the deployment")
 else:
-  print(query_answer.text)
+#  print(query_answer.text)
+  print("Contacting git....")
+  print("Got response...")
 
 
+model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
+
+json_schema = {
+    "title": "dockerfile",
+    "description": "Format of dockerfile links",
+    "type": "object",
+    "properties": {
+        "textresponse": {
+            "type": "string",
+            "description": "The text response portion",
+        },
+        "fileurl": {
+            "type": "string",
+            "description": "The actual url of the file",
+        },
+    },
+    "required": ["textresponse", "fileurl"],
+}
+
+structured_model = model.with_structured_output(json_schema)
+print("Initializing model...")
+response = structured_model.invoke(f'find dockerfiles in array {query_answer.text} return only value download_url')
+print("Found dockerfile...")
+#print(response)
+#print(response["fileurl"])
+
+print("Pulling dockerfile from git....")
+dockerfile = requests.get(response["fileurl"])
+docker_response = dockerfile.text
+first_line = docker_response.split('\n', 1)[0]
+print("Original: " + first_line)
+
+print("Sending entire dockerfile to LLM to determine latest baseimage...")
+response = model.invoke(f'Update the FROM command to be the latest baseimage version for {dockerfile.text}, return the updated dockerfile')
+print(response.content)
 
